a1c6d76bf6a3218b8635b0485c24435b7e030e8e
willabig <148019898+willabig@users.noreply.github.com>
2025-10-20 15:00:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
-pg = st.navigation([st.Page("streamlit_app_2.py"), st.Page("streamlit_app_3.py")])
+st.set_page_config(
+    page_title="Welcome Page",
+    page_icon="👋",
+)
+
+pg = st.navigation([st.Page("page_1.py"), st.Page("page_2.py")])
 pg.run()
