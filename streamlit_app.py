989301cc96c3a8304a68f362d802f0e71337c6d4
willabig <148019898+willabig@users.noreply.github.com>
2025-10-20 15:08:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,12 @@ st.set_page_config(
     page_icon="👋",
 )
 
-pg = st.navigation([st.Page("page_1.py"), st.Page("page_2.py")])
+# Define the pages
+page_1 = st.Page("page_1.py", title="Page 1", icon="❄️")
+page_2 = st.Page("page_2.py", title="Page 2", icon="🎉")
+
+# Set up navigation
+pg = st.navigation([page_1, page_2])
+
+# Run
 pg.run()
