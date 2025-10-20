30697ff670d1aef835d55f0cd9e8e460d8dafb7f
willabig <148019898+willabig@users.noreply.github.com>
2025-10-20 15:21:24 -0400

    Update page_1.py

--- pages/page_1.py
+++ pages/page_1.py
@@ -5,3 +5,8 @@ st.set_page_config(page_title="Config Basics")
 # Main page content
 st.markdown("# Config Basics")
 st.sidebar.markdown("# Config Basics")
+
+left, middle, right = st.columns(3, gap="medium", vertical_alignment="top", width="stretch")
+left.text_input("Xmin", key="xmin")
+middle.text_input("Xmax", key="xmax")
+right.text_input("dx", key="dx")
