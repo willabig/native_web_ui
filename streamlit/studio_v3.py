1bb32fc86b387eeb954cf92befb26ff3216d7936
willabig <willabig@iu.edu>
2025-11-14 14:41:24 -0500

    css

--- streamlit/studio_v3.py
+++ streamlit/studio_v3.py
@@ -20,10 +20,8 @@ import string
 config_tab, microenv_tab, celltypes_tab, userparams_tab, rules_tab, run_tab, plot_tab = st.tabs(["Config Basics", "Microenv", "Cell Types", "User Params", "Rules", "Run", "Plot"])
 st.set_page_config(layout="wide")
 
-with open('./studio.css') as f:
-    css = f.read()
 
-st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+st.markdown(f'<style>st.App{background-color: #ececec}</style>', unsafe_allow_html=True)
 
 #----------------------------------------------------------
 with config_tab:
